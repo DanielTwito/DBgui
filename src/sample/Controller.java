0f4257377a9e9e62bb211530e619622be67206e2
elad <eladc5492@gmail.com>
2018-10-16 18:50:04 +0300

    changes: - added a directory and a database file - added a method to validate existence of the database - added TODO lists

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,17 @@
 package sample;
 
+import java.sql.*;
+
 public class Controller {
-    //first commit for testing the connection
-    int x;
+
+    private Connection database;                            //connection object to hold the connection to the database
+
 }
+
+/*
+to do list:
+TODO: create method CREATE() <fill name of the dude whos going to do it>
+TODO: create method UPDATE() <fill name of the dude whos going to do it>
+TODO: create method READ() <fill name of the dude whos going to do it>
+TODO: create method REMOVE() <fill name of the dude whos going to do it>
+ */
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,7 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main extends Application {
+
+    // a boolean variable to enable various in code debug
+    static final boolean _DEBUG = true;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -18,6 +25,35 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        ValidateDatabase("Database/projectdb.db");
         launch(args);
     }
+
+    /**
+     * this method checks if a database exist, creates a new database in the given name if
+     * database does not exist.
+     * @param db_name - the name of the database we wish to check
+     */
+    public static void ValidateDatabase(String db_name)
+    {
+        Connection c = null;                                    //this object holds the connection to the database
+
+        File db = new File(db_name);    //a file object to point to the database file
+        if(!db.exists()) {                                      //checks if the database exist, if not than creates it
+            try {                                               //creates a new database inside the Database directory
+                c = DriverManager.getConnection("jdbc:sqlite:"+db_name);
+            } catch (Exception e) {
+                System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                System.exit(0);
+            }
+        }
+
+        if(_DEBUG)                                              //for debugging purposes
+            System.out.println("Opened database successfully");
+    }
 }
+
+/*
+to do list:
+TODO: switch _DEBUG to false at the end of the development
+ */
