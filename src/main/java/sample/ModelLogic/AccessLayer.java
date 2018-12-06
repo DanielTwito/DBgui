fbbdbc448d86724a4d4cb66817bccbd3a7821586
NetanelCohen241 <netanel2411@gmail.com>
2018-12-06 23:17:43 +0200

    Access Layer

--- src/main/java/sample/ModelLogic/AccessLayer.java
+++ src/main/java/sample/ModelLogic/AccessLayer.java
@@ -1,14 +1,30 @@
 package sample.ModelLogic;
 
+import com.sun.xml.internal.bind.v2.TODO;
 import sample.Enums.Fields;
 import sample.Enums.RESULT;
 import sample.Enums.Tables;
 
+import java.sql.Connection;
+import java.util.List;
+
 public class AccessLayer {
 
-    public RESULT AddEntry(String[] data, Tables table){return  null;}
-    public String[][] ReadEntry(String[] Key,Fields[]fields, Tables table){return null;}
+    private Connection connection;
+
+    public void connectDB(){}
+
+    //TODO:Twito
+    public RESULT AddEntry(List<String> data, Tables table){return  null;}
+    //TODO:Netanel
+    public String[][] ReadEntry(List<String> value, List<Fields> fields, Tables table){return null;}
+    //TODO:Netanel
     public RESULT UpdateEntry(Tables table, Fields fieldToUpdate, String newValue, Fields wantedField, String data){return null;}
-    public RESULT DeleteEntry (String[] key,Fields[]fields,Tables table){return null;}
+    //TODO:Twito
+    public RESULT DeleteEntry (List<String> value,List<Fields> fields,Tables table){return null;}
+
+    public void discoonetDB(){
+
+    }
 
 }
