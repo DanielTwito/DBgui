cb12ec1a20ef3dad9217269c3bc0a66c8acbc365
NetanelCohen241 <netanel2411@gmail.com>
2018-12-07 14:13:52 +0200

    AccessLayer- read and update

--- src/main/java/sample/ModelLogic/AccessLayer.java
+++ src/main/java/sample/ModelLogic/AccessLayer.java
@@ -1,6 +1,7 @@
 package sample.ModelLogic;
 
 import com.sun.xml.internal.bind.v2.TODO;
+import javafx.util.Pair;
 import sample.Enums.Fields;
 import sample.Enums.RESULT;
 import sample.Enums.Tables;
@@ -31,65 +32,96 @@ public class AccessLayer {
 
 
     /**
-     * This function reads an entries from DB according the given parameters.
+     * This function reads entries from DB according the given parameters.
      * Actually it executes a SELECT query command.
      * The function selects the all columns of the given table
-     * @param values- list of values that corresponds to fields list
-     * @param fields- list of fields to put in WHERE condition
      * @param table- "FROM" which table to read
+     * @param fieldsNvalues- list of fields and their values to put in "WHERE" condition
      * @return
      */
-    public HashMap<String, String> ReadEntries(ArrayList<String> values, ArrayList<String> fields, Tables table){
+    public ArrayList<HashMap<String, String>> ReadEntries(ArrayList<Pair> fieldsNvalues, Tables table){
 
-        HashMap<String, String> map = new HashMap<String, String>();
-        StringBuilder where = new StringBuilder();
-        where.append(fields.get(0)).append("=").append(values.get(0));
-        for (int i = 1; i < values.size(); i++) {
-            where.append("and").append(fields.get(i)).append("=").append(values.get(i));
-        }
-        try {
-            Statement query = connection.createStatement();
-            ResultSet result=query.executeQuery("SELECT * FROM "+table+" WHERE "+where+";");
+        ArrayList<HashMap<String, String>> ans = new ArrayList<>();
+        StringBuilder fields = new StringBuilder();
+        for (Pair fieldsNvalue : fieldsNvalues) fields.append(fieldsNvalue.getKey()).append("= ? and ");
+        String sql = "SELECT * FROM "+table+" WHERE "+fields;
+        sql=sql.substring(sql.length()-5);
+
+        try (PreparedStatement pstmt  = connection.prepareStatement(sql)){
+
+            for (int i = 1; i < fieldsNvalues.size(); i++)
+                pstmt.setString(1, (String) fieldsNvalues.get(i).getValue());
+
+            ResultSet result  = pstmt.executeQuery();
             ResultSetMetaData meta=result.getMetaData();
             int columnCount = meta.getColumnCount();
 
+
             // The column count starts from 1
             for (int i = 1; i <= columnCount; i++ ) {
+                HashMap<String, String> map = new HashMap<String, String>();
                 String name = meta.getColumnName(i);
                 map.put(name,result.getString(name));
+                ans.add(map);
             }
-        }
-        catch (Exception e){
-            System.out.println(e.getMessage());
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
-        return map;
+
+//        StringBuilder where = new StringBuilder();
+//        where.append(fieldsNvalues.get(0).getKey()).append("=").append(fieldsNvalues.get(0).getValue());
+//        for (int i = 1; i < fieldsNvalues.size(); i++) {
+//            where.append("and").append(fields.toString().get(i)).append("=").append(values.get(i));
+//        }
+//        try {
+//            Statement query = connection.createStatement();
+//            ResultSet result=query.executeQuery("SELECT * FROM "+table+" WHERE "+where+";");
+//            ResultSetMetaData meta=result.getMetaData();
+//            int columnCount = meta.getColumnCount();
+//
+//            // The column count starts from 1
+//            for (int i = 1; i <= columnCount; i++ ) {
+//                String name = meta.getColumnName(i);
+//                map.put(name,result.getString(name));
+//            }
+//        }
+//        catch (Exception e){
+//            System.out.println(e.getMessage());
+//        }
+
+        return ans;
     }
 
     /**
-     *
+     * This function updates entries on DB according the given parameters
      * @param table which table to update
      * @param fieldToUpdate- given field to update
-     * @param newValue-
-     * @param wantedField
-     * @param data
+     * @param newValue- the new value to assign to given field
+     * @param fieldsNvalues- list of fields and their values to put in "WHERE" condition
      * @return
      */
-    public RESULT UpdateEntries(Tables table, Fields fieldToUpdate, String newValue, Fields wantedField, String data){
+    public RESULT UpdateEntries(Tables table, String fieldToUpdate, String newValue, ArrayList<Pair> fieldsNvalues){
 
         RESULT out=RESULT.Success;
-        try
-        {
-            Statement query = connection.createStatement();                  // create a statement and execute the query
-            int result = query.executeUpdate("UPDATE "+table+" SET "+fieldToUpdate.toString()+" = '"+ newValue +"' WHERE "+ wantedField+" = '"+ data+"';");
-            if(result==0) {
+        StringBuilder fields = new StringBuilder();
+        for (Pair fieldsNvalue : fieldsNvalues) fields.append(fieldsNvalue.getKey()).append(" = ? and ");
+        String sql = "UPDATE "+table+" SET "+fieldToUpdate+" = "+newValue+" WHERE "+fields;
+        sql=sql.substring(sql.length()-5);
+
+        try (PreparedStatement pstmt  = connection.prepareStatement(sql)){
+
+            for (int i = 1; i < fieldsNvalues.size(); i++)
+                pstmt.setString(1, (String) fieldsNvalues.get(i).getValue());
+
+            int res=pstmt.executeUpdate();
+            if(res==0)
                 out = RESULT.Fail;
 
-            }
-        }
-        catch(Exception e) {
-            System.out.println(e.getMessage());
-            throw new NullPointerException();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
         return out;
