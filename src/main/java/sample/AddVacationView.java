44e81a7a847345abdbd5c1bd45798b2ee144921a
Avi Elly <eavi@post.bgu.ac.il>
2018-12-08 13:51:50 +0200

    --added register and signs to Access

--- src/main/java/sample/AddVacationView.java
+++ src/main/java/sample/AddVacationView.java
@@ -9,6 +9,7 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 import javafx.util.Pair;
 import sample.Enums.Fields;
+import sample.Enums.Tables;
 
 import java.time.LocalDate;
 import java.time.Period;
@@ -86,17 +87,18 @@ public class AddVacationView {
         if (msg.length() == 0) {
             ArrayList<Pair> vac = new ArrayList<>();
             vac.add(new Pair<>(Fields.vacationType, vacationTypeTXT.getText().trim()));
-            vac.add(new Pair<>(Fields.airline, AirLineTXT.getText().trim()));
+            vac.add(new Pair<>(Fields.airport, AirLineTXT.getText().trim()));
             vac.add(new Pair<>(Fields.destination, destinationTXT.getText().trim()));
-            vac.add(new Pair<>(Fields.FlightDate, startD.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")).trim()));
-            vac.add(new Pair<>(Fields.ReturnDate, endD.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")).trim()));
+            vac.add(new Pair<>(Fields.Flydate, startD.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")).trim()));
+            vac.add(new Pair<>(Fields.Returndate, endD.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")).trim()));
             vac.add(new Pair<>(Fields.adultTickets, adultAmount.getValue().toString()));
             vac.add(new Pair<>(Fields.childTickets, ChildAmount.getValue().toString()));
             vac.add(new Pair<>(Fields.babyTickets, Babyamount.getValue().toString()));
-            vac.add(new Pair<>(Fields.Baggage, LuggageType.getValue()));
+            vac.add(new Pair<>(Fields.baggage, LuggageType.getValue()));
             vac.add(new Pair<>(Fields.placeRank, roomRank.getValue()));
             vac.add(new Pair<>(Fields.includeRoom, withStuff.isSelected()));
             vac.add(new Pair<>(Fields.includeReturn, withReturn.isSelected()));
+            control.AddEntry(vac,Tables.ListingVacations);
         } else {
             errorBoard.setText(msg.toString());
         }
