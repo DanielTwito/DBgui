8f9764a76ca23ca2113c0da7ae675bea6fc9eeb0
DanielTwito <Daniel.twito7@gmail.com>
2018-12-09 13:14:21 +0200

    fields update

--- src/main/java/sample/Enums/Fields.java
+++ src/main/java/sample/Enums/Fields.java
@@ -1,4 +1,4 @@
 package sample.Enums;
 
 public enum Fields {userName, password, birthDate, firstName, lastName,Email, city,image,airport,Flydate,Returndate,baggage,adultTickets,childTickets,babyTickets
-    ,destination,includeReturn,vacationType,includeRoom,placeRank,price,homeTown,VacID}
+    ,destination,includeReturn,vacationType,includeRoom,placeRank,price,homeTown,VacID,creditCard,expirationDate,security,balance}
